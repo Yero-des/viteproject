8e00a95390889f4004eb88cfe532ff3f4566839a
Yero-des <yero_zc@hotmail.com>
2026-03-04 22:44:31 -0500

    Pequeños cambios

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views
 
 app_name = 'dashboard'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="dashboard/index.html"), name="index")
+    path('', views.IndexTemplateView.as_view(), name="index")
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class IndexTemplateView(TemplateView):
+    template_name = 'dashboard/index.html'
+    
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get('name')
+        context.update({
+            'name': name
+        })
+        return context
+    
